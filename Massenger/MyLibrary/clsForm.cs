62b34a06ca74f5119fa1dde8f7d25ed395174661
Marrwan03 <tlooddi107@gmail.com>
2024-08-02 14:20:29 +0300

    Add files via upload

---
+++ Massenger/MyLibrary/clsForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyLibrary
+{
+    public partial class clsForm : Form
+    {
+        public clsForm()
+        {
+            InitializeComponent();
+        }
+
+        public static void SetForm(Form frm, int Width, int Height)
+        {
+            frm.Size = new Size(Width, Height);
+            frm.Location = new Point(0, 0);
+        }
+
+        private void clsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
