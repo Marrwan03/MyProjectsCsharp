cb2dd707963d88d965bbb3b589aab983ccceef5f
Marrwan03 <tlooddi107@gmail.com>
2024-09-14 07:06:54 +0300

    Add files via upload

---
+++ DVLD-Project(My solution)/Main Forms/frmLoginSystem.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO;
+using System.Data;
+
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Collections;
+using clsBusinessTier;
+using MyLibrary;
+using System.Drawing;
+
+namespace DVLD_Project
+{
+    public partial class frmLoginSystem : Form
+    {
+        public frmLoginSystem()
+        {
+            InitializeComponent();
+        }
+        public static frmLoginSystem frmLogin;
+        clsBusinessUsers _CurrentUser;
+
+       public void RefreshLoginData(string UserName,  string Password)
+        {
+            txtUsername.Text = UserName;
+            txtPassword.Text = Password;
+        }
+
+        bool setLoginDataInTextBoxes()
+        {
+            string Path = @"C:\Users\lenovo\OneDrive\Desktop\LoginData\";
+            if (File.Exists(Path + "LoginData.txt"))
+            {
+                string LoginData = File.ReadAllText(Path + "LoginData.txt");
+                string[] Data = LoginData.Split('#');
+                txtUsername.Text = Data[0];
+                txtPassword.Text = Data[1];
+                return true;
+            }
+           
+            return false;
+        }
+
+       bool Check(string Username, string Password)
+        {
+            _CurrentUser = clsBusinessUsers.Find(Username, Password);
+            if( _CurrentUser != null )
+            {
+                return true;
+            }
+            return false;
+        }
+
+        void _RefreshLoginData()
+        {
+            if (setLoginDataInTextBoxes())
+            {
+                chbrememberme.Checked = true;
+                lblTitleLogin.Text = "We cannot forget you,\nWelcome Back :-)";
+            }
+            else
+            {
+                lblTitleLogin.Text = "Login to your account...";
+                txtUsername.Text = string.Empty;
+                txtPassword.Text = string.Empty;
+            }
+        }
+
+        private void frmLoginSystem_Load(object sender, EventArgs e)
+        {
+            frmLogin = this;
+            _RefreshLoginData();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(!Check(txtUsername.Text, txtPassword.Text))
+            {
+                MessageBox.Show("Invalid Username / Password", "Wrong Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if(!_CurrentUser.IsActive)
+            {
+                MessageBox.Show("You haven`t access in this system", "Wrong Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if(chbrememberme.Checked)
+            {
+                clsFiles.LoginDataInFile(txtUsername.Text, txtPassword.Text,false, true);
+            }
+            else
+            {
+                clsFiles.LoginDataInFile(txtUsername.Text, txtPassword.Text, true, false);
+            }
+
+            frmLogin.Hide();
+            frmMain mainScreen = new frmMain(_CurrentUser.UserID);
+            mainScreen.DataBack += _RefreshLoginData;
+            mainScreen.Show();
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmLogin.Close();
+        }
+
+        private void txtPassword_MouseEnter(object sender, EventArgs e)
+        {
+            clsForm.ShowPassword(sender, e);
+            clsColorControl.TextBox_MouseEnter(sender, e);
+        }
+
+        private void txtPassword_MouseLeave(object sender, EventArgs e)
+        {
+            clsForm.HidePassword(sender, e, '*');
+            clsColorControl.TextBox_MouseLeave(sender, e);
+        }
+
+        public  void Button_MouseEnter(object sender, EventArgs e)
+        {
+           clsColorControl.Button_MouseEnter(sender, e);
+        }
+
+        public  void Button_MouseLeave(object sender, EventArgs e)
+        {
+            clsColorControl.Button_MouseLeave(sender, e);
+        }
+        public  void Label_MouseEnter(object sender, EventArgs e)
+        {
+          clsColorControl.Label_MouseEnter(sender, e);
+        }
+
+        public  void Label_MouseLeave(object sender, EventArgs e)
+        { 
+           clsColorControl.Label_MouseLeave(sender, e);
+        }
+        public  void CheckBox_MouseEnter(object sender, EventArgs e)
+        {
+           clsColorControl.CheckBox_MouseEnter(sender, e);
+        }
+        public  void CheckBox_MouseLeave(object sender, EventArgs e)
+        {
+           clsColorControl.CheckBox_MouseLeave(sender, e);
+        }
+
+        public  void TextBox_MouseEnter(object sender, EventArgs e)
+        {
+            clsColorControl.TextBox_MouseEnter(sender, e);
+        }
+        public  void TextBox_MouseLeave(object sender, EventArgs e)
+        {
+           clsColorControl.TextBox_MouseLeave(sender, e);
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DVLD-Project(My solution)/Main Forms/frmMain.cs
@@ -0,0 +1,287 @@
+﻿using clsBusinessTier;
+using MyLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD_Project
+{
+    public partial class frmMain : Form
+    {
+        public void ToolStripMenuItem_MouseEnter(object sender, EventArgs e)
+        {
+          clsColorControl.ToolStripMenuItem_MouseEnter(sender, e);
+        }
+        public void ToolStripMenuItem_MouseLeave(object sender, EventArgs e)
+        {
+            clsColorControl.ToolStripMenuItem_MouseLeave(sender, e);
+        }
+
+
+        public delegate void MyDelegate();
+        public event MyDelegate DataBack;
+
+        void _RefreshCurrentUser()
+        {
+            clsGlobalSettings.CurrentUser = clsBusinessUsers.Find(_CurrentUserID);
+        }
+        int _CurrentUserID;
+        public frmMain(int UserID)
+        {
+            InitializeComponent();
+            _CurrentUserID = UserID;
+            clsGlobalSettings.CurrentUser = clsBusinessUsers.Find(_CurrentUserID);
+        }
+        void _CloseAllForms(bool CloseMainForm = false)
+        {
+            if(frmPeople != null)
+                frmPeople.Close();
+            if(frmusers != null)
+                frmusers.Close();
+            if(userDetails != null)
+                userDetails.Close();
+            if(changePassword != null)
+                changePassword.Close();
+            if(manageApplicationTypes != null)
+                manageApplicationTypes.Close();
+            if(manageTestTypes != null)
+                manageTestTypes.Close();
+            if(localDrivingLicenseApplications != null)
+                localDrivingLicenseApplications.Close();
+            if(newLocalDrivingLicensesApplication  != null)
+                newLocalDrivingLicensesApplication.Close();
+            if(listDrivers  != null)
+                listDrivers.Close();
+            if(newInternationalDrivingLicenseApplicaiton!=null)
+                newInternationalDrivingLicenseApplicaiton.Close();
+            if(listIntLIcenseApp != null)
+                listIntLIcenseApp.Close();
+            if (renewLicenseApplication != null)
+                renewLicenseApplication.Close();
+            if(replacementforDamagedorLostLicenses != null)
+                replacementforDamagedorLostLicenses.Close();
+            if (releaseDetainLicense != null)
+                releaseDetainLicense.Close();
+            if(listDetainedLicenses != null)
+                listDetainedLicenses.Close();
+            if(detainLicense != null)
+                detainLicense.Close();
+
+            if (CloseMainForm)
+                this.Close();
+
+
+        }
+
+        public frmPeople frmPeople ;
+        private void peToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            frmPeople = new frmPeople();
+            frmPeople.MdiParent = this;
+            frmPeople.Show();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        frmUserDetails userDetails;
+        private void currentUserToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            userDetails = new frmUserDetails(clsGlobalSettings.CurrentUser);
+            userDetails.Show();
+            //_RefreshCurrentUser();
+        }
+
+        private void signToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Are you sure, \nDo you want Sign out?","Sign Out!", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                _CloseAllForms(true);
+                clsGlobalSettings.CurrentUser = clsBusinessUsers.Find(clsGlobalSettings.CurrentUser.UserID);
+                DataBack?.Invoke();
+                frmLoginSystem.frmLogin.Show();
+            }
+        }
+
+        frmUsers frmusers;
+        private void usersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            frmusers = new frmUsers();
+          
+            frmusers.Show();
+          
+        }
+
+        frmChangePassword changePassword;
+        private void changePasswordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            changePassword = new frmChangePassword(clsGlobalSettings.CurrentUser);
+            changePassword.MdiParent = this;
+            changePassword.Show();
+           
+        }
+
+        frmManageApplicationTypes manageApplicationTypes;
+        private void manageApplicationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            manageApplicationTypes = new frmManageApplicationTypes();
+            manageApplicationTypes.MdiParent= this;
+            manageApplicationTypes.Show();
+        }
+
+        frmManageTestTypes manageTestTypes;
+        private void manageTestTypesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            manageTestTypes = new frmManageTestTypes();
+            manageTestTypes.MdiParent=this;
+            manageTestTypes.Show();
+        }
+
+        frmLocalDrivingLicenseApplications localDrivingLicenseApplications;
+        private void localDrivingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            localDrivingLicenseApplications = new frmLocalDrivingLicenseApplications();
+            localDrivingLicenseApplications.MdiParent = this;
+            localDrivingLicenseApplications.Show();
+        }
+
+        private void newDrivingLicensesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        frmAdd_UpdateLocalDrivingLicensesApplication newLocalDrivingLicensesApplication;
+        private void localLicensesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            newLocalDrivingLicensesApplication = new frmAdd_UpdateLocalDrivingLicensesApplication(-1);
+            newLocalDrivingLicensesApplication.MdiParent = this;
+            newLocalDrivingLicensesApplication.Show();
+
+        }
+
+        frmListDrivers listDrivers;
+        private void driversToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            listDrivers = new frmListDrivers();
+            listDrivers.MdiParent = this;
+            listDrivers.Show();
+        }
+
+        private void accountSettingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        frmNewInternationalDrivingLicenseApplicaiton newInternationalDrivingLicenseApplicaiton;
+        private void internationalLicensesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            newInternationalDrivingLicenseApplicaiton = new frmNewInternationalDrivingLicenseApplicaiton();
+            newInternationalDrivingLicenseApplicaiton.MdiParent = this;
+            newInternationalDrivingLicenseApplicaiton.Show();
+
+
+        }
+
+        frmListIntLIcenseApp listIntLIcenseApp;
+        private void internationalLicenseApplicationsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            listIntLIcenseApp = new frmListIntLIcenseApp();
+            listIntLIcenseApp.MdiParent = this;
+            listIntLIcenseApp.Show();
+        }
+
+        
+
+
+        frmDetainLicense detainLicense;
+        private void detainLicenseToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            detainLicense = new frmDetainLicense();
+            detainLicense.MdiParent = this;
+            detainLicense.Show();
+        }
+
+        frmReleaseDetainLicense releaseDetainLicense;
+        private void releaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            releaseDetainLicense = new frmReleaseDetainLicense(-1);
+            releaseDetainLicense.MdiParent = this;
+            releaseDetainLicense.Show();
+        }
+
+        frmListDetainedLicenses listDetainedLicenses;
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            listDetainedLicenses = new frmListDetainedLicenses();
+            listDetainedLicenses.MdiParent = this;
+            listDetainedLicenses.Show();
+        }
+
+        private void releaseToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            releaseDetainLicense = new frmReleaseDetainLicense(-1);
+            releaseDetainLicense.MdiParent = this;
+            releaseDetainLicense.Show();
+        }
+
+        private void retakeTestToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            localDrivingLicenseApplications = new frmLocalDrivingLicenseApplications();
+            localDrivingLicenseApplications.MdiParent = this;
+            localDrivingLicenseApplications.Show();
+        }
+
+        private void applicationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void detainLicenseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        frmRenewLicenseApplication2 renewLicenseApplication;
+        private void renewDrivingLicensesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            renewLicenseApplication = new frmRenewLicenseApplication2();
+            renewLicenseApplication.MdiParent = this;
+            renewLicenseApplication.Show();
+        }
+
+        frmReplacementforDamagedorLostLicenses22 replacementforDamagedorLostLicenses;
+        private void replacementForDamagedOrLostLicensesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _CloseAllForms();
+            replacementforDamagedorLostLicenses = new frmReplacementforDamagedorLostLicenses22();
+            replacementforDamagedorLostLicenses.MdiParent = this;
+            replacementforDamagedorLostLicenses.Show();
+
+        }
+    }
+}
