1e95ca2452d6814ce269505f7736f8b0e473f012
Marrwan03 <tlooddi107@gmail.com>
2024-09-14 07:06:08 +0300

    Add files via upload

---
+++ DVLD-Project(My solution)/GlobalClass/clsGlobalSettings.cs
@@ -0,0 +1,11 @@
+﻿using clsBusinessTier;
+using System;
+using System.Data;
+
+namespace DVLD_Project
+{
+    public class clsGlobalSettings
+    {
+        public static clsBusinessUsers CurrentUser { get; set; }
+    }
+}
