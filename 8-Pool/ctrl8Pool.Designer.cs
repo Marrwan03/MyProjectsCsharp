8903c380b02adc98b0218c97f8882e1ae2d789a9
Marrwan03 <tlooddi107@gmail.com>
2024-12-17 10:27:28 +0300

    Add files via upload

---
+++ 8-Pool/ctrl8Pool.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace FirsGUNA
+{
+    partial class ctrl8Pool
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gbTable = new Guna.UI2.WinForms.Guna2GroupBox();
+            this.cmsTable = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.nameOfTableToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.typeOfTableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cbTypeOfTable = new System.Windows.Forms.ToolStripComboBox();
+            this.designToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorOfHeaderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorOfBodyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorOfSwitchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnEnd = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.btnSwitch = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.picTable = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.cdControl = new System.Windows.Forms.ColorDialog();
+            this.gbTable.SuspendLayout();
+            this.cmsTable.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbTable
+            // 
+            this.gbTable.BackColor = System.Drawing.Color.Transparent;
+            this.gbTable.BorderColor = System.Drawing.Color.SlateBlue;
+            this.gbTable.ContextMenuStrip = this.cmsTable;
+            this.gbTable.Controls.Add(this.lblTimer);
+            this.gbTable.Controls.Add(this.btnEnd);
+            this.gbTable.Controls.Add(this.btnSwitch);
+            this.gbTable.Controls.Add(this.picTable);
+            this.gbTable.CustomBorderColor = System.Drawing.Color.Green;
+            this.gbTable.FillColor = System.Drawing.Color.DarkSlateGray;
+            this.gbTable.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gbTable.ForeColor = System.Drawing.Color.White;
+            this.gbTable.Location = new System.Drawing.Point(3, 3);
+            this.gbTable.Name = "gbTable";
+            this.gbTable.Size = new System.Drawing.Size(374, 257);
+            this.gbTable.TabIndex = 0;
+            this.gbTable.Text = "Programming Advice";
+            this.gbTable.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cmsTable
+            // 
+            this.cmsTable.ImageScalingSize = new System.Drawing.Size(50, 50);
+            this.cmsTable.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nameOfTableToolStripMenuItem1,
+            this.typeOfTableToolStripMenuItem,
+            this.designToolStripMenuItem});
+            this.cmsTable.Name = "cmsTable";
+            this.cmsTable.Size = new System.Drawing.Size(187, 172);
+            this.cmsTable.Opening += new System.ComponentModel.CancelEventHandler(this.cmsTable_Opening_1);
+            // 
+            // nameOfTableToolStripMenuItem1
+            // 
+            this.nameOfTableToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1});
+            this.nameOfTableToolStripMenuItem1.Image = global::FirsGUNA.Properties.Resources.NameOfTable;
+            this.nameOfTableToolStripMenuItem1.Name = "nameOfTableToolStripMenuItem1";
+            this.nameOfTableToolStripMenuItem1.Size = new System.Drawing.Size(186, 56);
+            this.nameOfTableToolStripMenuItem1.Text = "Name Of Table";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.AutoSize = false;
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            this.toolStripTextBox1.TextChanged += new System.EventHandler(this.toolStripTextBox1_TextChanged);
+            // 
+            // typeOfTableToolStripMenuItem
+            // 
+            this.typeOfTableToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cbTypeOfTable});
+            this.typeOfTableToolStripMenuItem.Image = global::FirsGUNA.Properties.Resources.TypeOfTable;
+            this.typeOfTableToolStripMenuItem.Name = "typeOfTableToolStripMenuItem";
+            this.typeOfTableToolStripMenuItem.Size = new System.Drawing.Size(186, 56);
+            this.typeOfTableToolStripMenuItem.Text = "Type Of Table";
+            this.typeOfTableToolStripMenuItem.Click += new System.EventHandler(this.typeOfTableToolStripMenuItem_Click);
+            // 
+            // cbTypeOfTable
+            // 
+            this.cbTypeOfTable.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbTypeOfTable.Items.AddRange(new object[] {
+            "Normal",
+            "VIP"});
+            this.cbTypeOfTable.Name = "cbTypeOfTable";
+            this.cbTypeOfTable.Size = new System.Drawing.Size(121, 23);
+            this.cbTypeOfTable.SelectedIndexChanged += new System.EventHandler(this.cbTypeOfTable_SelectedIndexChanged);
+            // 
+            // designToolStripMenuItem
+            // 
+            this.designToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorOfHeaderToolStripMenuItem,
+            this.colorOfBodyToolStripMenuItem,
+            this.colorOfSwitchToolStripMenuItem});
+            this.designToolStripMenuItem.Image = global::FirsGUNA.Properties.Resources.Design;
+            this.designToolStripMenuItem.Name = "designToolStripMenuItem";
+            this.designToolStripMenuItem.Size = new System.Drawing.Size(186, 56);
+            this.designToolStripMenuItem.Text = "Design";
+            // 
+            // colorOfHeaderToolStripMenuItem
+            // 
+            this.colorOfHeaderToolStripMenuItem.Name = "colorOfHeaderToolStripMenuItem";
+            this.colorOfHeaderToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.colorOfHeaderToolStripMenuItem.Text = "Color Of Header";
+            this.colorOfHeaderToolStripMenuItem.Click += new System.EventHandler(this.colorOfHeaderToolStripMenuItem_Click);
+            // 
+            // colorOfBodyToolStripMenuItem
+            // 
+            this.colorOfBodyToolStripMenuItem.Name = "colorOfBodyToolStripMenuItem";
+            this.colorOfBodyToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.colorOfBodyToolStripMenuItem.Text = "Color Of Body";
+            this.colorOfBodyToolStripMenuItem.Click += new System.EventHandler(this.colorOfBodyToolStripMenuItem_Click);
+            // 
+            // colorOfSwitchToolStripMenuItem
+            // 
+            this.colorOfSwitchToolStripMenuItem.Name = "colorOfSwitchToolStripMenuItem";
+            this.colorOfSwitchToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.colorOfSwitchToolStripMenuItem.Text = "Color Of Switch";
+            this.colorOfSwitchToolStripMenuItem.Click += new System.EventHandler(this.colorOfSwitchToolStripMenuItem_Click);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.lblTimer.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.ForeColor = System.Drawing.Color.White;
+            this.lblTimer.Location = new System.Drawing.Point(278, 204);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(88, 21);
+            this.lblTimer.TabIndex = 3;
+            this.lblTimer.Text = "00 : 00 : 00";
+            this.lblTimer.Click += new System.EventHandler(this.lblTimer_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnEnd.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnEnd.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnEnd.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnEnd.FillColor = System.Drawing.Color.Green;
+            this.btnEnd.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnEnd.ForeColor = System.Drawing.Color.White;
+            this.btnEnd.Location = new System.Drawing.Point(298, 127);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.btnEnd.Size = new System.Drawing.Size(54, 47);
+            this.btnEnd.TabIndex = 2;
+            this.btnEnd.Text = "End";
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnSwitch.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnSwitch.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnSwitch.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnSwitch.FillColor = System.Drawing.Color.Green;
+            this.btnSwitch.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnSwitch.ForeColor = System.Drawing.Color.White;
+            this.btnSwitch.Location = new System.Drawing.Point(298, 74);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.btnSwitch.Size = new System.Drawing.Size(54, 47);
+            this.btnSwitch.TabIndex = 1;
+            this.btnSwitch.Tag = "On";
+            this.btnSwitch.Text = "Start";
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // picTable
+            // 
+            this.picTable.Cursor = System.Windows.Forms.Cursors.Help;
+            this.picTable.Image = global::FirsGUNA.Properties.Resources.QuestionMark;
+            this.picTable.Location = new System.Drawing.Point(4, 44);
+            this.picTable.Name = "picTable";
+            this.picTable.Size = new System.Drawing.Size(268, 193);
+            this.picTable.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picTable.TabIndex = 0;
+            this.picTable.TabStop = false;
+            this.picTable.Tag = "?";
+            this.picTable.DoubleClick += new System.EventHandler(this.picTable_DoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ctrl8Pool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gbTable);
+            this.Name = "ctrl8Pool";
+            this.Size = new System.Drawing.Size(380, 263);
+            this.gbTable.ResumeLayout(false);
+            this.gbTable.PerformLayout();
+            this.cmsTable.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox picTable;
+        private Guna.UI2.WinForms.Guna2CircleButton btnEnd;
+        private Guna.UI2.WinForms.Guna2CircleButton btnSwitch;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer timer1;
+        public Guna.UI2.WinForms.Guna2GroupBox gbTable;
+        private System.Windows.Forms.ToolStripMenuItem nameOfTableToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem typeOfTableToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog cdControl;
+        private System.Windows.Forms.ToolStripMenuItem designToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorOfHeaderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorOfBodyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorOfSwitchToolStripMenuItem;
+        public System.Windows.Forms.ToolStripComboBox cbTypeOfTable;
+        public System.Windows.Forms.ContextMenuStrip cmsTable;
+    }
+}
---
+++ 8-Pool/ctrl8Pool.cs
@@ -0,0 +1,385 @@
+﻿using FirsGUNA.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirsGUNA
+{
+    public partial class ctrl8Pool : UserControl
+    {
+        public ctrl8Pool()
+        {
+            InitializeComponent();
+        }
+        // 60 Mins -> 10 $
+        // 1 Min   -> 0.1666666666666667 $
+        // 1 Sec   -> 0.0027777777777778 $
+
+        byte _HourlyRate = 10;
+
+      
+       public enum enStatusGame { Start, Stop, End}
+        public string NameOfTable { get { return gbTable.Text; } set { gbTable.Text = value; } }
+        public enum enTypeOfTable { None, Normal, VIP};
+        enTypeOfTable _TypeOfTable;
+        public enTypeOfTable TypeOfTable { get { return _TypeOfTable; } set 
+            {
+                _TypeOfTable = value;
+
+                switch (_TypeOfTable)
+                {
+                    case enTypeOfTable.None:
+                        {
+                            picTable.Tag = "?";
+                            picTable.Image = Resources.QuestionMark;
+                            return;
+                        }
+                        case enTypeOfTable.Normal:
+                        {
+                            cbTypeOfTable.Text = "Normal";
+                            break;
+                        }
+                        case enTypeOfTable.VIP:
+                        {
+                            cbTypeOfTable.Text = "VIP";
+                            break;
+                        }
+                }
+
+
+                _ChangePicture(false);
+            } 
+        }
+        public Color ColorOfHeader { get { return gbTable.CustomBorderColor; } set { gbTable.CustomBorderColor = value; } }
+        public Color ColorOfBody { get { return gbTable.FillColor; } 
+            set 
+            {
+                gbTable.FillColor = value;
+                lblTimer.BackColor = value;
+            } }
+        public Color ColorOfbtnEnd
+        {
+            get { return btnEnd.FillColor; }
+            set
+            {
+                btnEnd.FillColor = value;
+            }
+        }
+        public Color ColorOfbtnStart
+        {
+            get { return btnSwitch.FillColor; }
+            set
+            {
+                btnSwitch.FillColor = value;
+            }
+        }
+        public static int _GetTotalSeconds(DateTime dt)
+        {
+            int Total = dt.Hour * 60 * 60;
+            Total += dt.Minute * 60;
+            Total += dt.Second;
+            
+            return Total;
+        }
+
+       static double _GetPrice(int TotalSeconds, double SecondlyRate)
+        {
+            return TotalSeconds * SecondlyRate;
+        }
+    
+     
+        public class clsEvent8Pool : EventArgs
+        {
+            public string NameOfTable { get; }
+            public enStatusGame statusGame { get; }
+            public string StringStatusGame {
+                get
+                {
+                    switch (statusGame)
+                    {
+                        case enStatusGame.Start:
+                            return "Start";
+                            case enStatusGame.Stop:
+                            return "Stop";
+                        default:
+                            return "End";
+                    }
+
+                }
+            }
+            public DateTime Date { get; }
+            public int TotalSeconds { get; }
+            public double Price { get; }
+            public string TypeOfTable { get; }
+
+            public clsEvent8Pool(string NameOfTable,enTypeOfTable typeOfTable,enStatusGame statusGame, DateTime date)
+            {
+                this.statusGame = statusGame;
+                Date = date;
+                this.NameOfTable = NameOfTable;
+                
+                TotalSeconds = _GetTotalSeconds(date);
+              
+                
+                if(typeOfTable == enTypeOfTable.Normal)
+                {
+                    TypeOfTable = "Normal";
+                Price = _GetPrice(TotalSeconds, 0.0027777777777778);
+                }
+                else
+                {
+                    TypeOfTable = "VIP";
+                    Price = _GetPrice(TotalSeconds, 0.0083333333333334);
+                }
+            }
+        }
+
+
+        public event EventHandler<clsEvent8Pool> OnStart8Pool;
+        public event EventHandler<clsEvent8Pool> OnStop8Pool;
+        public event EventHandler<clsEvent8Pool> OnEnd8Pool;
+
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+            if(picTable.Tag == "?")
+            {
+                MessageBox.Show("Set Table Information to play", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+            if (Tag == null)
+                Tag = "On";
+
+            if(Tag == "On")
+            {
+                Tag = "Off";
+                btnSwitch.Text = "Stop";
+                timer1.Start();
+                OnStart8Pool?.Invoke(this,new clsEvent8Pool(NameOfTable, _TypeOfTable,enStatusGame.Start, new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, PartHours,PartMinutes, PartSeconds)));
+            }
+            else
+            {
+                Tag = "On";
+                btnSwitch.Text = "Start";
+                timer1.Stop();
+                OnStop8Pool?.Invoke(this, new clsEvent8Pool(NameOfTable, _TypeOfTable, enStatusGame.Stop, new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, PartHours, PartMinutes, PartSeconds)));
+            }
+
+            _ChangePicture(true);
+
+            nameOfTableToolStripMenuItem1.Enabled = false;
+            typeOfTableToolStripMenuItem.Enabled = false;
+            
+        }
+
+
+        byte PartHours = 0, PartMinutes = 0, PartSeconds = 0;
+
+        void _ChangePicture(bool WithLamp)
+        {
+            picTable.Tag = "!";
+            switch (cbTypeOfTable.Text)
+            {
+                case "Normal":
+                    {
+                        
+                        if (WithLamp)
+                            picTable.Image = Resources.Normal_Table_With_Lamp;
+                        else
+                            picTable.Image = Resources.Normal_Table_Without_Lamp;
+
+                        break;
+                    }
+                default:
+                    {
+                        if (WithLamp)
+                            picTable.Image = Resources.VIP_Table_With_Lamp;
+                        else
+                            picTable.Image = Resources.VIP_Table_Without_Lamp;
+                        break;
+                    }
+                   
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //_Date.AddSeconds(1);
+            //_Date.Add(new TimeSpan(0, 0, 0, 1));
+            //DateTime dt = _Date; 
+            PartSeconds++;
+            if (PartMinutes == 60)
+            {
+                PartMinutes = 0;
+                ++PartHours;
+            }
+            if(PartSeconds == 60)
+            {
+                ++PartMinutes;
+                PartSeconds = 0;
+            }
+            
+
+            lblTimer.Text = $"{PartHours} : {PartMinutes} : {PartSeconds}";
+
+        }
+        void _RefreshData()
+        {
+
+            picTable.Image = Resources.QuestionMark;
+            picTable.Tag = "?";
+            gbTable.Text = "Programming Advice";
+            gbTable.CustomBorderColor = Color.Green;
+            gbTable.FillColor = Color.DarkSlateGray;
+            lblTimer.BackColor = Color.DarkSlateGray;
+            btnSwitch.FillColor = Color.Green;
+            btnEnd.FillColor = Color.Green;
+            PartHours = 0;
+            PartMinutes = 0;
+            PartSeconds = 0;
+            lblTimer.Text = "00 : 00 : 00";
+            btnSwitch.Text = "Start";
+            btnSwitch.Tag = "On";
+            nameOfTableToolStripMenuItem1.Enabled = true;
+            typeOfTableToolStripMenuItem.Enabled = true;
+            cbTypeOfTable.SelectedIndex = -1;
+           // _ChangePicture(false);
+        }
+
+       
+
+        
+
+        private void toolStripTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            NameOfTable = toolStripTextBox1.Text;
+        }
+
+        private void cmsTable_Opening_1(object sender, CancelEventArgs e)
+        {
+            toolStripTextBox1.Text = NameOfTable;
+        }
+
+      
+
+
+
+        void _TableInfo(string TypeOfTable, int HournltRate)
+        {
+            MessageBox.Show($@"Type Of Table is : {TypeOfTable},
+
+Hourly Rate       : {HournltRate}.", "Table Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void picTable_DoubleClick(object sender, EventArgs e)
+        {
+            switch (cbTypeOfTable.Text)
+            {
+                case "Normal":
+                    {
+                        _TableInfo("Normal", 10);
+                        break;
+                    }
+                case "VIP":
+                    {
+                        _TableInfo("VIP", 30);
+                        break;
+                    }
+                default:
+                    {
+                        _TableInfo("NULL", 00);
+                        break;
+                    }
+                   
+            }
+        }
+
+        private void cbTypeOfTable_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void colorOfHeaderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(cdControl.ShowDialog() == DialogResult.OK)
+            {
+                gbTable.CustomBorderColor = cdControl.Color;
+            }
+        }
+
+        private void colorOfBodyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (cdControl.ShowDialog() == DialogResult.OK)
+            {
+                gbTable.FillColor = cdControl.Color;
+                lblTimer.BackColor = cdControl.Color;
+            }
+        }
+
+        private void colorOfSwitchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (cdControl.ShowDialog() == DialogResult.OK)
+            {
+                btnSwitch.FillColor = cdControl.Color;
+                btnEnd.FillColor = cdControl.Color;
+            }
+        }
+
+        private void typeOfTableToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        private void cbTypeOfTable_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            picTable.Tag = "!";
+            switch (cbTypeOfTable.Text)
+            {
+                case "Normal":
+                    {
+                        picTable.Image = Resources.Normal_Table_Without_Lamp;
+                       // _TableInfo("Normal", 10);
+                        break;
+                    }
+                case "VIP":
+                    {
+                        picTable.Image = Resources.VIP_Table_Without_Lamp;
+                       // _TableInfo("VIP", 30);
+                        break;
+                    }
+                default:
+                    {
+                        picTable.Tag = "?";
+                        //_TableInfo("NULL", 00);
+                        break;
+                    }
+
+            }
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            if(picTable.Tag=="!")
+            {
+                timer1.Stop();
+                OnEnd8Pool?.Invoke(this, new clsEvent8Pool(NameOfTable, _TypeOfTable, enStatusGame.End, new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, PartHours, PartMinutes, PartSeconds)));
+                _RefreshData();
+            }
+           
+        }
+
+        private void lblTimer_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
